48a3cacb4b12a27181752bd98f2d82c9b9194e97
Aditya Prawira <adityaprawira96@gmail.com>
2021-09-28 14:07:31 +1000

    Added thread lock method for sfae synchronization

--- camera_stream.py
+++ camera_stream.py
@@ -1,6 +1,5 @@
-from threading import Thread
+from threading import Thread, Lock
 import cv2 
-import time
 
 # Task for the class: The class will have a thread that constantly gathering frames
 # 1. launch a camera -> do it one time, hence create a constructor for this
@@ -11,15 +10,22 @@ class CameraStream:
     self.width = width # width of display window
     self.height = height # height of display window
     self.capture = cv2.VideoCapture(cameraSrc) # launched the camera
+    self.threadStarted = True
     self.thread = Thread(target=self.update, args=()) # launched the thread that will continuously collects frame
-    self.thread.daemon = True # kills the thread when the program is terminated
+    self.readLock = Lock()
+    # self.thread.daemon = True # kills the thread when the program is terminated
     self.thread.start() # start thread during initialization
-    
+
   def update(self): # the class method will continuously reading frames from the passed camera
-    while True:
+    while self.threadStarted:
       _, self.frame = self.capture.read()
+      self.readLock.acquire()
       self.newFrame = cv2.resize(src = self.frame, dsize=(self.width, self.height)) # resize window to the given dimension
+      self.readLock.release()
       
-  def getFrame(self):
-    return self.newFrame # return the new frame that has been resized
   
+  def getFrame(self): return self.newFrame # return copy of the new frame that has been resized
+  
+  def stopStreamThread(self):
+    self.threadStarted = False
+    self.thread.join()
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Note: Cameras are synch with threads used in CameraStream class
 import cv2
 import numpy as np
+import time
 from camera_stream import CameraStream
+# lowpass filter constants
+DELTA_T_AVERAGE_WEIGHT = 0.9
+DELTA_T_WEIGHT = 0.1
+deltaTimeAverage = 0
 
+# display constants
 displayWidth = 640
 displayHeight = 480
 
@@ -14,20 +20,41 @@ cam1 = CameraStream(LAPTOP_CAM_CODE, displayWidth, displayHeight)
 WEB_CAM_CODE = 1
 cam2 = CameraStream(WEB_CAM_CODE, displayWidth, displayHeight)
 
+# setup to display timer in the display window
+font = cv2.FONT_HERSHEY_SIMPLEX
+startTime = time.time() # calculate starting time
+
+
 while True:
   try: # try the program again if it has trouble when reading frames
     frame1 = cam1.getFrame()
     frame2 = cam2.getFrame()
     combinedFrame = np.hstack((frame1, frame2))
+
+    currentTime = time.time() # get the current time in the loop
+    deltaTime = currentTime - startTime # calculate the time required to complete the task of getting frames from multi-camera
+    startTime = time.time() # update the value of startTime
+    
+    # Setting up lowpass filter to not trust the collected data in the case then the deltaTime recorded is too big/slow
+    # hence, setting up 10% weight of the deltaTime
+    deltaTimeAverage = DELTA_T_AVERAGE_WEIGHT*deltaTimeAverage + DELTA_T_WEIGHT*deltaTime 
+    
+    # calculate fps
+    fps = round(1/deltaTimeAverage, 1) # frame persecond will be calculated using the lowpass filtered change of time
+    cv2.rectangle(combinedFrame, (0,0), (150, 40), (0,0,0), -1)
+    cv2.putText(combinedFrame, str(fps)+" FPS", (0, 25), font, 0.75, (255, 255, 255))
     cv2.imshow("Laptop Camera and Webcam Camera", combinedFrame)
-    cv2.moveWindow("Laptop Camera and Webcam Camera", 0, 0)
+    # print(f"Reading Time: {deltaTimeAverage} seconds")
   except:
-    print("Frame not available: Trying to reconnect")
+    print("Frame from CameraStream not available: Trying to reconnect")
     
   # Option to exit the program gracefully
   if cv2.waitKey(1) == ord('q'):
     # release camera
+    cam1.stopStreamThread()
     cam1.capture.release()
+    
+    cam2.stopStreamThread()
     cam2.capture.release()
     cv2.destroyAllWindows()
     exit(1) # killing all threads
